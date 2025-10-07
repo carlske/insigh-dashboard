6f46df5ec0d9296de7d67f7257329ac446fc13ef
carlske <fernancarldiaz@outlook.com>
2025-10-07 15:10:44 -0600

    chore: removed old dependencys

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { env } from "../../config/env";
 import { AppError } from "../../core/errors/AppError";
